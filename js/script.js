ea1d336e959c25c05722cb251d0cc0de741400d8
OlehRadchenko <or435654@gmail.com>
2022-10-09 17:04:50 +0400

    Changed_place_script_on_HTML

--- js/script.js
+++ js/script.js
@@ -1,2 +1,2 @@
-document.getElementById("a").innerHTML = "Jakie lorem ipsum, tu strona ma być? (())__(())";
+document.getElementById("a").innerHTML  = "Jakie lorem ipsum, tu strona ma być? (())__(())";
 console.log("Test");
